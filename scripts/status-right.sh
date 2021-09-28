4ea59b5adc186befa857000712355dab44d1a6a3
Nicky Bell <nicky_zs@163.com>
2021-09-28 15:06:22 +0800

    Update status-right.sh

--- scripts/status-right.sh
+++ scripts/status-right.sh
@@ -6,7 +6,7 @@ dir_name=$(dirname $0)
 . $dir_name/helpers.sh
 
 if [[ $(get_width) -gt 200 ]]; then
-	echo "#(~/.tmux/bin/resource_usage)#[bg=colour6] #(date +'%a %Y/%m/%d %H:%M:%S') "
+	echo "$(~/.tmux/bin/resource_usage)#[bg=colour6] $(date +'%a %Y/%m/%d %H:%M:%S') "
 else
-	echo "#(~/.tmux/bin/resource_usage narrow)#[bg=colour6] #(date +'%m/%d %H:%M:%S') "
+	echo "$(~/.tmux/bin/resource_usage narrow)#[bg=colour6] $(date +'%m/%d %H:%M:%S') "
 fi
