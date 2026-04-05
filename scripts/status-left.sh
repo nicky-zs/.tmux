d00cf58ad2bd576350e545c6743f8d56eec5fb6e
Zhong Sheng <zhongsheng@liepin.com>
2026-04-05 14:28:15 +0800

    use dash instead of bash

--- scripts/status-left.sh
+++ scripts/status-left.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
-set -ue
+set -eu
 
-dir_name=$(dirname $0)
-. $dir_name/helpers.sh
+dir_name=$(dirname "$0")
+. "$dir_name/helpers.sh"
 
-if [[ $(get_width) -gt 160 ]]; then
+if [ "$(get_width)" -gt 160 ]; then
 	echo " #H:#S #I:#P "
 else
 	echo " #S "
--- scripts/status-right.sh
+++ scripts/status-right.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
-set -ue
+set -eu
 
-dir_name=$(dirname $0)
-. $dir_name/helpers.sh
+dir_name=$(dirname "$0")
+. "$dir_name/helpers.sh"
 
-if [[ $(get_width) -gt 200 ]]; then
+if [ "$(get_width)" -gt 200 ]; then
 	echo "$(~/.tmux/bin/resource_usage)#[bg=colour6] $(date +'%a %Y/%m/%d %H:%M:%S') "
 else
 	echo "$(~/.tmux/bin/resource_usage narrow)#[bg=colour6] $(date +'%m/%d %H:%M:%S') "
